a0c14ed1dc69721cb26902aca8082737e6721ded
JudahPraise <judahpraiseocampo@gmail.com>
2021-04-25 01:28:28 +0800

    first app

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My First App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Center(
+          child: Text('This is body'),
+        ),
+      ),
+    );
+  }
+}
